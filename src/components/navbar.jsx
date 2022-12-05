d9fa644540c9d8fc540f5234b84922e1d6491e8f
Alessandro Vitali <alessandro.vitali@consensys.net>
2022-12-05 15:28:17 +0100

    track user account

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,35 @@
+import { ethers } from "ethers";
 import Link from "next/link";
+import { useState } from "react";
+import { useEffect } from "react";
+import { formatAddress } from "../utils";
 
 export default function Navbar() {
+  const [accounts, setAccounts] = useState([]);
+  useEffect(() => {
+    if (!window.ethereum) return;
+    const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+    function handleAccountsChanged(accounts) {
+      setAccounts(accounts);
+    }
+
+    provider.provider.on("accountsChanged", handleAccountsChanged);
+
+    async function getAccounts() {
+      const accounts = await provider.listAccounts();
+      setAccounts(accounts);
+    }
+
+    getAccounts();
+
+    return () => {
+      provider.provider.removeListener(
+        "accountsChanged",
+        handleAccountsChanged
+      );
+    };
+  }, []);
+
   return (
     <nav className="border-b p-6">
       <p className="text-4xl font-bold">Metaverse Marketplace</p>
@@ -17,6 +46,9 @@ export default function Navbar() {
         <Link href="/dashboard" className="mr-6 text-pink-500">
           Dashboard
         </Link>
+        <div className="ml-auto">
+          {accounts.length ? formatAddress(accounts[0]) : "not connected"}
+        </div>
       </div>
     </nav>
   );
--- src/utils/index.js
+++ src/utils/index.js
@@ -3,3 +3,12 @@ export const LoadingStates = {
   NotLoaded: "not-loaded",
   Loaded: "loaded",
 };
+
+/**
+ * Format an Ethereum address
+ * @param {string} addr - Address to format
+ * @returns string - Formatted address
+ */
+export function formatAddress(addr) {
+  return `${addr.substring(0, 6)}...${addr.substring(addr.length - 4)}`;
+}
